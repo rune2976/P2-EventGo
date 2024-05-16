31d18bd437f4077526be1253273be9f29c24cc5b
dragonslayer9000 <60519923+dragonslayer9000@users.noreply.github.com>
2024-05-16 19:50:02 +0200

    ollolo

--- EventGo/logout.js
+++ EventGo/logout.js
@@ -5,3 +5,17 @@ const logoutBtn = document.querySelector(".logout-btn")
 logoutBtn.addEventListener("click",()=>{
     window.location.replace("login.html")
 })
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer');
+    var scrollPosition = window.scrollY;
+    var windowHeight = window.innerHeight;
+    var documentHeight = document.body.clientHeight;
+  
+    // Viser footer'en, hvis brugeren er i bunden af siden
+    if (scrollPosition + windowHeight >= documentHeight) {
+      footer.style.display = 'block';
+    } else {
+      footer.style.display = 'none';
+    }
+  });
